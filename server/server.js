fd82dfafd5675260bdf3c6255c8172d2533bd2ce
lairmartes <lairmartes@gmail.com>
2018-02-08 06:19:26 -0200

    Including message utils and chat is working in browser

--- server/server.js
+++ server/server.js
@@ -19,9 +19,10 @@ io.on('connection', function(socket) {
 
     socket.broadcast.emit('newMessage', generateMessage('Admin', 'new user joined'));
 
-    socket.on('createMessage', (message) => {
+    socket.on('createMessage', (message, callback) => {
         console.log('createMessage', message);
         io.emit('newMessage', generateMessage(message.from, message.text));
+        callback('This is from the server.');
     });
 
 
