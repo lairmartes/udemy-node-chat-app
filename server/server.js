b821f532ca907804988c531d75a49139c23d0c0a
lairmartes <lairmartes@gmail.com>
2018-02-09 07:06:41 -0200

    Add css for chat page

--- server/server.js
+++ server/server.js
@@ -19,6 +19,12 @@ io.on('connection', function(socket) {
 
     socket.broadcast.emit('newMessage', generateMessage('Admin', 'new user joined'));
 
+    socket.on('createMessage', (message, callback) => {
+        console.log('createMessage', message)
+        io.emit('newMessage', generateMessage(message.from, message.text));
+        callback();
+    });
+
     socket.on('createLocationMessage', (coords) => {
         io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
     });
